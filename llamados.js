1104d074baac99e0613a069679f1851a2e8211e4
César Osses <cesar.osses@gmail.com>
2021-07-09 15:33:37 -0400

    Pokedex dia 1

---
+++ llamados.js
@@ -0,0 +1,35 @@
+$("#main_card").hide()
+
+function buscarPokemon() {
+    $("#main_card").show()
+    nombre_pokemon = $("#txt-busqueda").val()
+    fetch("https://pokeapi.co/api/v2/pokemon/" + nombre_pokemon)
+        .then(respuesta => respuesta.json())
+        .then(datos => {
+            nueva_etiqueta = `
+            <img src="${datos.sprites.front_default}">
+        `
+            $("#pokemon_image").attr("src", datos.sprites.front_default)
+
+            $("#nombre-pokemon").text(datos.species.name)
+
+            $(".card-text").html(`
+            <p>
+            N° : ${datos.id} 
+            <br> 
+            Altura: ${datos.height}
+            <br>
+            Peso: ${datos.weight}
+            <br>
+            Ataques: ${datos.moves[0].move.name}, ${datos.moves[1].move.name}, ${datos.moves[2].move.name}, ${datos.moves[3].move.name}, ${datos.moves[4].move.name}
+            <br>
+            </p>
+            `)
+
+            
+        
+        
+        })
+        .catch(error => console.log(error))
+
+}
